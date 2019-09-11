7bf484a211c8a77de938281dead9d3e3092d3f0b
JazzyMussels <jeffmussels@gmail.com>
2019-09-10 21:26:42 -0400

    cleaned up line passing methods

--- bin/run.rb
+++ bin/run.rb
@@ -5,32 +5,54 @@ require 'pry'
 require 'tty-prompt'
 
 ActiveRecord::Base.logger.level = 1 
-prompt = TTY::Prompt.new
+@prompt = TTY::Prompt.new
 
-
-
-def run
 greeting
-create_user
+create_user #add option to log-in w/ passcode
+look_at_options
 select_provider
 set_location
 pickup_lines_for_location
 random_line
-interaction_with_provider
-end
+#first_pass
+#accept_or_pass?
+#second_pass
+#accept_or_pass?
+#final_pass
+fire_provider
+disable_provider
+retry?
+
+
+
+
+
+
+
+
+
+# def run
+# greeting
+# create_user
+# select_provider
+# set_location
+# pickup_lines_for_location
+# random_line
+# interaction_with_provider
+# end
 
   
 
 
 
-# if rejected, OR success line of logic
+#  if rejected, OR success line of logic
 
 
-puts prompt.select("Your Provider has been fired. Would you like to try again?" , %w(Choose\ another\ location  Exit))
+# puts prompt.select("Your Provider has been fired. Would you like to try again?" , %w(Choose\ another\ location  Exit))
 
 
 
-binding.pry
+# binding.pry
 
 
-puts 'k'
+# puts 'k'
--- lib/app/models/app_process.rb
+++ lib/app/models/app_process.rb
@@ -1,12 +1,16 @@
+
+
+#wrap a prompt as a class_var and change all
+
 def greeting
   puts "Welcome to The Real-Time, In a Bind, Online, Pickup Line Hotline!"
 end
 
 def create_user
-  input = prompt.select("Asuuuh?", %w(Create\ Profile Exit))
+  input = @prompt.select("Asuuuh?", %w(Create\ Profile Exit))
     if input == 'Create Profile'
-      name = prompt.ask("What is your name?", required: true){|q| q.modify :capitalize}
-      phone = prompt.ask("What is your Phone Number", required: true)
+      name = @prompt.ask("What is your name?", required: true){|q| q.modify :capitalize}
+      phone = @prompt.ask("What is your Phone Number", required: true)
       User.create(name: name, phone_number: phone)
     else
       exit_program
@@ -15,7 +19,7 @@ end
 
 def look_at_options
   options = ['Get a line', 'See what worked', "Lives you've ruined"]
-  input = prompt.select("So What's good?", options)
+  input = @prompt.select("So What's good?", options)
     if input == options[0]
       select_provider
     elsif input == options[1]
@@ -28,13 +32,13 @@ def look_at_options
 end
 
 def select_provider
-  provider_selection = prompt.select("Which one of these assholes you want helping you?", %w(Chad Tanner Braxton Brenadeigh Kavaleigh DeAnnalynn Exit))
+  provider_selection = @prompt.select("Which one of these assholes you want helping you?", %w(Chad Tanner Braxton Brenadeigh Kavaleigh DeAnnalynn Exit))
   exit_program if provider_selection == 'Exit'
   self.update(provider_id: Provider.find_by(name: self).id)
 end
 
 def set_location
-  location_selection = prompt.select("Choose your location", ['Hogwarts', 'Mos Eisley Cantina', 'The Gym', 'A Coffee Shop', 'The Dog Park', 'The Beach or Pool', 'A Programming Meetup', 'Democratic National Convention', 'A Bar', 'Another Location', 'Exit'])
+  location_selection = @prompt.select("Choose your location", ['Hogwarts', 'Mos Eisley Cantina', 'The Gym', 'A Coffee Shop', 'The Dog Park', 'The Beach or Pool', 'A Programming Meetup', 'Democratic National Convention', 'A Bar', 'Another Location', 'Exit'])
   exit_program if provider_selection == 'Exit'
   self.update(location_id: Location.find_by(name: location_selection).id)
 end
@@ -47,68 +51,94 @@ def random_line
   pickup_lines_for_location.sample.content
 end
 
+#def begin_generation
+  #@prompt.keypress('Cool, chill for a sec while we find something solid...', timeout:3)
+  #puts ''
+#end
+
+#def attempt_line(msg) = "Alright, so based on your location, here's a pretty solid line:", 'my b on that, try this:', 'Ok, this will deff work'
+#   puts ''
+#   @prompt.keypress( msg, timeout:1)
+#   puts ''
+#   puts '===================='
+#   random_line
+#   puts '===================='
+#   puts ''
+# end
+
+# def accept_or_pass?(msg)   = 'Did they buy it?', "If this doesn't work this person is whack."
+#   answer = @prompt.select(msg, %w(fershur nawww))
+#     if answer == 'nawww'
+#         return false
+#     else
+#         puts 'TAAAAIGHT!'
+              #self.accepted_lines << random_line        #that needs fixing
+# end
+
+
+  
 def interaction_with_provider
-    prompt.keypress('Cool, chill for a sec while we find something solid...', timeout:3)
+    @prompt.keypress('Cool, chill for a sec while we find something solid...', timeout:3)
   puts ''
-  prompt.keypress("Alright, so based on your location, here's a pretty solid line:", timeout:1)
-  prompt.keypress('', timeout:1)
+  @prompt.keypress("Alright, so based on your location, here's a pretty solid line:", timeout:1)
+  @prompt.keypress('', timeout:1)
   puts '===================='
   puts sample
   puts '===================='
   puts ''
-  answer1 = prompt.select('Did they buy it?', %w(fershur nawww))
+  answer1 = @prompt.select('Did they buy it?', %w(fershur nawww))
        if answer1 == 'nawww'
         puts ''
-          prompt.keypress('my b on that, try this:', timeout: 1)
+          @prompt.keypress('my b on that, try this:', timeout: 1)
           puts ''
         puts '===================='
           sample = users_location_lines.sample.content 
           puts sample
           puts '===================='
           puts ''
-          answer2 = prompt.select('did they buy it?', %w(fershur nawww))
+          answer2 = @prompt.select('did they buy it?', %w(fershur nawww))
                 if answer2 == 'nawww'
                   puts ''
-                   prompt.keypress('Ok, this will deff work', timeout:1)
+                   @prompt.keypress('Ok, this will deff work', timeout:1)
                    puts ''
                    puts '===================='
                    sample = users_location_lines.sample.content 
                    puts sample
                    puts '===================='
                    puts ''
-                   answer3 = prompt.select("If this doesn't work this person is whack.", %w(fershur nawww))
+                   answer3 = @prompt.select("If this doesn't work this person is whack.", %w(fershur nawww))
                       puts '' 
                    if answer3 == 'nawww'
                       puts ''
-                     prompt.keypress("Wooooooow, it's like that?", timeout: 1)
+                     @prompt.keypress("Wooooooow, it's like that?", timeout: 1)
                     else
                      puts 'TAAAAIGHT!'
-                      self.accepted_lines <<sample
+                      self.accepted_lines << sample
                     end
                 else  
                   puts 'TAAAAIGHT!'
-                   self.accepted_lines <<sample
+                   self.accepted_lines << sample
                 end
         else  
            puts 'TAAAAIGHT!'
-            self.accepted_lines <<sample
+            self.accepted_lines << sample
         end
 end
 
 @@fired_providers = []
 
 def fire_provider?
-  input = prompt.select("We have noticed you have rejected 3 of our Pickup Lines, should we fire your provider?" , %w(Yes No))
+  input = @prompt.select("We have noticed you have rejected 3 of our Pickup Lines, should we fire your provider?" , %w(Yes No))
       if input == 'Yes'
         @@fired_providers << self.provider.name
         fired = Provider.find_by(name: self.provider)
         fired.delete
-        prompt.multiline("Write your complaint here:")
-        prompt.keypress("Reading complaint....", timeout:4)
-        prompt.keypress("You're right, they sucked, firing your provider now....", timeout:3)
-        prompt.keypress("They took it pretty okay, said they're gonna go finish school or something!", timeout:2)
+        @prompt.multiline("Write your complaint here:")
+        @prompt.keypress("Reading complaint....", timeout:4)
+        @prompt.keypress("You're right, they sucked, firing your provider now....", timeout:3)
+        @prompt.keypress("They took it pretty okay, said they're gonna go finish school or something!", timeout:2)
       else
-        return false
+        puts 'very chill of you'
       end
 end
 
@@ -120,7 +150,7 @@ def disable_provider
         hash[:disabled] = "(fired)"
   end
   arr1 << hash 
-  prompt.select('Pick a new provider', arr1)
+  @prompt.select('Pick a new provider', arr1)
 end
 
 def self.all_that_worked
@@ -132,7 +162,7 @@ def self.all_that_worked
 end
 
 def retry?
-  response = prompt.select("Would you like to try again?", %w(Choose\ another\ provider  Exit))
+  response = @prompt.select("Would you like to try again?", %w(Choose\ another\ provider  Exit))
   if response == 'Choose another provider'
     disable_provider
   elsif response == 'Exit'
